035a2fea1149580ee4f4d5702173d64e2ee2860d
danieleldjarn <danieleldjarn@gmail.com>
2017-10-25 19:27:59 +0200

    Display text on screen

--- microweather.ino
+++ microweather.ino
@@ -1,18 +1,22 @@
 /*****************************************************************
-MicroWeather station
+  MicroWeather station
 
-A little weather app for my MicroView.
-Gets weather information form internet and displays on screen.
+  A little weather app for my MicroView.
+  Gets weather information form internet and displays on screen.
 *****************************************************************/
 #include <MicroView.h>
 
 
+
 void setup() {
   // put your setup code here, to run once:
-
+  uView.begin();
+  uView.clear(PAGE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  uView.setCursor(10, 10);
+  uView.print("Temp [C]");
+  uView.display();
 }
