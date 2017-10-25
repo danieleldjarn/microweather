30976d37bb140acd7f24829e981123ad12a438da
danieleldjarn <danieleldjarn@gmail.com>
2017-10-25 18:35:24 +0200

    main program

---
+++ microweather.ino
@@ -0,0 +1,18 @@
+/*****************************************************************
+MicroWeather station
+
+A little weather app for my MicroView.
+Gets weather information form internet and displays on screen.
+*****************************************************************/
+#include <MicroView.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
